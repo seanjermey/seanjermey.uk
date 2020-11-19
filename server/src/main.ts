f8c55d1ff78bf8b8544583189734c37079e382f5
Sean Jermey <seanjermey87@gmail.com>
2020-11-19 14:28:16 +0000

    amends

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,13 @@ import * as config from 'config';
 async function bootstrap(port) {
   const app = await NestFactory.create(AppModule);
 
+  if (process.env.NODE_ENV !== 'production') {
+    app.enableCors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+    });
+  }
+
   await app.listen(port);
 }
 
